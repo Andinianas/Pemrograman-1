68371781f2daf0a22962209402e169e7075f3d9a
Andinianas <andiniputrianas@gmail.com>
2014-10-07 15:29:51 +0700

    genap

---
+++ Pertemuan 1/genap.java
@@ -0,0 +1,11 @@
+//Program - 02
+//Nama	: Andini Putri
+//NPM	: 4513210006
+public class genap{
+	public static void main(String[] args){
+		int i;
+		for(i=0;i<20;i+=2){
+			System.out.println("i=" + i);
+		}
+	}
+}
